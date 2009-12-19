b6272a00bad106cfddcf65668c8b9cb782068d8e
rayapudisandeep <rayapudisandeep@3cafc7d8-ecc9-11de-a75d-bbceaaa5444e>
2009-12-19 18:30:29 +0000

    From: http://java.sun.com/developer/technicalArticles/J2SE/jmx.html
    After running first set of programs from "The JMX Technology Programming Model" section
    
    To experiment with this application, do the following:
    
       1. Create a directory of your choice (such as jmx-example)
       2. Copy Code Samples 1, 2, and 3 into that directory
       3. Compile all the .java files using javac
       4. Run SimpleAgent. In order to use the jconsole tool to manage it, you should run the SimpleAgent as follows:

---
+++ src/myjmx/Hello.java
@@ -0,0 +1,25 @@
+package myjmx;
+
+public class Hello implements HelloMBean {
+   private String message = null;
+
+   public Hello() {
+      message = "Hello there";
+   }
+
+   public Hello(String message) {
+      this.message = message;
+   }
+
+   public void setMessage(String message) {
+      this.message = message;
+   }
+
+   public String getMessage() {
+      return message;
+   }
+
+   public void sayHello() {
+      System.out.println(message);
+   }
+}
